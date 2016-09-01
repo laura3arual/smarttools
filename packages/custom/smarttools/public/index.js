8f0106d43571a201312ec9b3827875a1a59944c2
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2016-09-01 05:02:22 -0500

    Estilos del landing page y contenido

--- packages/custom/smarttools/public/index.js
+++ packages/custom/smarttools/public/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import './assets/css/common.css';
+//import './assets/css/common.css';
 import './assets/css/loginForms.css';
 import './assets/css/starter.css';
+import './assets/css/landing-page.css';
 import 'ng-file-upload';
