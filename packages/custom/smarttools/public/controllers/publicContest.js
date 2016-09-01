819a8b7b6acbec158b361d08cbc941a3cbc3967c
laura3arual <laura3arual@gmail.com>
2016-09-01 07:44:06 -0500

    se crea un servidor para video streaming y se consume desde la vista de videos

--- packages/custom/smarttools/public/controllers/publicContest.js
+++ packages/custom/smarttools/public/controllers/publicContest.js
@@ -1,13 +1,23 @@
 'use strict';
 
-angular.module('mean.system').controller('PublicContestController', ['$scope', 'Global', 'Contest', '$state', '$stateParams', '$http',
-  function($scope, Global, Contest, $state, $stateParams, $http) {
+angular.module('mean.system').controller('PublicContestController', ['$scope', 'Global', 'Contest', '$state', '$stateParams', '$http', '$sce',
+  function($scope, Global, Contest, $state, $stateParams, $http, $sce) {
+    $scope.htmlVideoTags = {};
     $scope.currentContestId = $stateParams['contestId'];
     $scope.loadVideos = function(){
       $http.get('/api/public/contests/' + $stateParams['contestId']).then(function(response){
         $scope.videos = response.data;
+        $scope.setHtmlTags($scope.videos);
       });
     }
     $scope.loadVideos();
+
+    $scope.setHtmlTags = function(videos){
+      videos.forEach(function(video){
+        $scope.htmlVideoTags[video._id] = $sce.trustAsHtml(
+          ' <video controls><source src="http://localhost:9000/' + video._id + '.mp4" type="video/mp4">' +
+          'Your browser does not support the video tag.</video>');
+      });
+    }
   }
 ]);
