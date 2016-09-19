d6ea256ec18f227bce26aca69e7e26161750ceec
Laura Ariza <laura3arual@gmail.com>
2016-09-18 20:39:43 -0500

    entrega 1 fixes

--- packages/custom/smarttools/public/controllers/updateContest.js
+++ packages/custom/smarttools/public/controllers/updateContest.js
@@ -1,12 +1,25 @@
 'use strict';
 
-angular.module('mean.system').controller('UpdateContestController', ['$scope', 'Global', 'Contest', '$state', '$stateParams',
-  function($scope, Global, Contest, $state, $stateParams) {
+angular.module('mean.system').controller('UpdateContestController', ['$scope', 'Global', 'Contest', '$state', '$stateParams', 'Upload',
+  function($scope, Global, Contest, $state, $stateParams, Upload) {
+    $scope.ready = false;
     $scope.currentContest = Contest.get({contestId: $stateParams['contestId']});
+    $scope.currentContest.$promise.then(function(data){
+      $scope.currentContest.startDate = new Date(data.startDate);
+      $scope.currentContest.endDate = new Date(data.endDate);
+      $scope.ready = true;
+    });
+
     $scope.updateContest = function(){
      this.currentContest.$update(function (){
           $state.go('contests');
         });
     }
+
+    $scope.upload = function (file) {
+       Upload.base64DataUrl(file).then(function(urls){
+         $scope.currentContest.image = urls;
+       });
+   };
   }
 ]);
